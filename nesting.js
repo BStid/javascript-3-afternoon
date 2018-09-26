f2269c2c09d62a570652a691a2ad6ec80e85f21f
Ben <ben@benstidham.com>
2018-09-26 15:38:54 -0500

    Completed All Exercises

--- nesting.js
+++ nesting.js
@@ -50,7 +50,31 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+
+// employeeUpdater = () => {
+//   for(let i = 0; i< employees.length;i++){
+//     if(employees[i].firstName === "Theo"){
+//       delete employees[i]
+//     }
+//     else if(employees[i].firstName === 'Lorie'){
+//       employees[i].department = 'HR';
+//     }
+//   }
+//   return employees;
+// }
+
+employeeUpdater = () => {
+  employees.map((value,index,array) =>{
+    if(value.firstName === "Theo"){
+      array.splice(index,1);
+    }
+    else if(value.firstName === 'Lorie'){
+      value.department = 'HR';
+    }
+  })
+  return employees;
+}
+  
 
 
 
@@ -68,7 +92,17 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+removeDuplicates = (workplaceAccidents) => {
+  for(let i = 0; i< workplaceAccidents.length; i++){
+    for(let j = workplaceAccidents.length - 1; j>=0; j--){
+      if(workplaceAccidents[i] === workplaceAccidents[j] && i !== j){
+        workplaceAccidents.splice(j,1);
+      }
+      }
+    }
+    return workplaceAccidents;
+  }
+
 
 
 
@@ -97,8 +131,9 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
+
 
 
 
@@ -138,7 +173,27 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
+const recordCleaner = () => {
+  for(let i=0; i<myCar.accidents.length;i++){
+    if(myCar.accidents[i].atFaultForAccident === true){
+      myCar.accidents[i].atFaultForAccident = false;
+    }
+  }
+  return myCar
+}
+
+console.log(myCar);
+
+// const recordCleaner = arr => {
+//     for(let props in arr){
+//       if(props === "atFaultForAccident"){
+//         for(let key in )
+//       }
+//     }
+//     return arr
+//   }
+  
+//   console.log(myCar);
 
 
 
@@ -157,6 +212,20 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
+let looper = () => {
+  console.log(numsArr);
+  for(let i = 0; i < numsArr.length; i++ ){
+    
+    for(let j=0;j<numsArr[i].length;j++)
+    {
+      if(numsArr[i][j] % 2 === 0){
+        numsArr[i][j] = 'even'
+      }else{
+        numsArr[i][j] = 'odd';
+      }
+    }
+  }
+  return numsArr;
+}
 
 
